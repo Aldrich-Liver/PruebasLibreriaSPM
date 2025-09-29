7f5fd3089b8deff363d06eed07eb342a7015ff78
Aldrich Gonzalez <agonzalezg31@liverpool.com.mx>
2025-09-29 09:52:45 -0600

    Initial Commit

---
+++ PruebasLibreriaSPM/PruebasLibreriaSPMApp.swift
@@ -0,0 +1,17 @@
+//
+//  PruebasLibreriaSPMApp.swift
+//  PruebasLibreriaSPM
+//
+//  Created by ALDRICH GONZALEZ GOMEZ on 29/09/25.
+//
+
+import SwiftUI
+
+@main
+struct PruebasLibreriaSPMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
